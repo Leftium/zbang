5b98e70265865b52c5d5102c54d7561b1b73ce6b
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-11-02 09:23:47 +0900

    Refactor autosizing textarea.

--- kit/src/lib/components/AutogrowingTextarea.svelte
+++ kit/src/lib/components/AutogrowingTextarea.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	//// Based on: https://css-tricks.com/the-cleanest-trick-for-autogrowing-textareas
-
 	import type { HTMLTextareaAttributes, FormEventHandler } from 'svelte/elements'
 
 	let {
@@ -18,9 +16,17 @@
 			oninput(event)
 		}
 
-		if (this.parentNode) {
-			;(this.parentNode as HTMLElement).dataset.replicatedValue = this.value
-		}
+		const lineHeight = parseFloat(window.getComputedStyle(this).lineHeight)
+		const maxLines = 6
+
+		// Reset the height to auto to get the correct scrollHeight
+		this.style.height = 'auto'
+
+		const height = this.scrollHeight
+		this.style.height = Math.min(height, lineHeight * maxLines) + 'px'
+
+		// Toggle scrollbar based on number of lines
+		this.style.overflowY = height > lineHeight * maxLines ? 'scroll' : 'hidden'
 	}
 </script>
 
@@ -31,38 +37,9 @@
 
 <style lang="scss">
 	grow-wrap {
-		// Easy way to plop the elements on top of each other and have them both sized based on the tallest one's height
-		display: grid;
-
-		&::after {
-			// Note the weird space! Needed to preventy jumpy behavior
-			content: attr(data-replicated-value) ' ';
-
-			// This is how textarea text behaves
-			white-space: pre-wrap;
-
-			// Hidden from view, clicks, and screen readers
-			visibility: hidden;
-		}
-
-		& > textarea {
-			// You could leave this, but after a user resizes, then it ruins the auto sizing
-			resize: none;
-
-			// Firefox shows scrollbar on growth, you can hide like this.
-			overflow: hidden;
-		}
-
-		& > textarea,
-		&::after {
-			// Identical styling required!!
-			border: 1px solid;
-			padding: 0.75rem 1rem;
-			margin: 0;
-			font: inherit;
-
-			// Place on top of each other
-			grid-area: 1 / 1 / 2 / 2;
+		textarea {
+			white-space: nowrap;
+			font-family: monospace;
 		}
 	}
 </style>
