fea24bd78e8a76f876c3e5e66d5ecfe23245e1a2
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-11-14 12:40:14 +0900

    Don't use oclif prettier rules.

--- bang-tools/src/commands/download.ts
+++ bang-tools/src/commands/download.ts
@@ -1,35 +1,35 @@
-import {Command, Flags} from '@oclif/core'
+import { Command, Flags } from '@oclif/core'
 import jetpack from 'fs-jetpack'
 
 const urls = [
-  'https://duckduckgo.com/bang.js',
-  'https://github.com/kagisearch/bangs/raw/refs/heads/main/data/bangs.json',
-  'https://github.com/kagisearch/bangs/raw/refs/heads/main/data/kagi_bangs.json',
-  'https://github.com/kagisearch/bangs/raw/refs/heads/main/data/assistant_bangs.json',
+	'https://duckduckgo.com/bang.js',
+	'https://github.com/kagisearch/bangs/raw/refs/heads/main/data/bangs.json',
+	'https://github.com/kagisearch/bangs/raw/refs/heads/main/data/kagi_bangs.json',
+	'https://github.com/kagisearch/bangs/raw/refs/heads/main/data/assistant_bangs.json',
 ]
 
 const filenames = urls.map((url) => url.split('/').at(-1))
 filenames[0] = 'duckduckgo_bangs.json'
 
 export default class Download extends Command {
-  static override description = 'Download bang files.'
+	static override description = 'Download bang files.'
 
-  static override flags = {
-    // flag with a value (-n, --name=VALUE)
-    outputDir: Flags.string({char: 'o', description: 'output directory'}),
-  }
+	static override flags = {
+		// flag with a value (-n, --name=VALUE)
+		outputDir: Flags.string({ char: 'o', description: 'output directory' }),
+	}
 
-  public async run(): Promise<void> {
-    const {flags} = await this.parse(Download)
+	public async run(): Promise<void> {
+		const { flags } = await this.parse(Download)
 
-    const outputDir = flags.outputDir ?? 'bangs'
-    this.log(`output directory: ${outputDir}`)
+		const outputDir = flags.outputDir ?? 'bangs'
+		this.log(`output directory: ${outputDir}`)
 
-    const fetchedAll = await Promise.all(urls.map((url) => fetch(url)))
-    const textedAll = await Promise.all(fetchedAll.map((fetched) => fetched.text()))
+		const fetchedAll = await Promise.all(urls.map((url) => fetch(url)))
+		const textedAll = await Promise.all(fetchedAll.map((fetched) => fetched.text()))
 
-    for (const [index, texted] of textedAll.entries()) {
-      jetpack.write(`${outputDir}/${filenames[index]}`, texted)
-    }
-  }
+		for (const [index, texted] of textedAll.entries()) {
+			jetpack.write(`${outputDir}/${filenames[index]}`, texted)
+		}
+	}
 }
