ce53641bae7f3580204f8048d0f6a09f88c17227
Leftium <381217+Leftium@users.noreply.github.com>
2025-09-20 19:10:56 -0500

    Refactor: update to latest Kit template.

--- kit/src/routes/+layout.svelte
+++ kit/src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import '$src/app.scss'
+	import favicon from '$lib/assets/favicon.svg'
+
+	let { children } = $props()
 </script>
 
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
 <main class="container">
-	<slot />
+	{@render children?.()}
 </main>
