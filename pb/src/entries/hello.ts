281e2c6fb8d2bd1c92c91582f18ec57e85306663
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-01-01 14:36:13 +0900

    chore: remove pocketbase stuff.

--- pb/src/entries/hello.ts
+++
@@ -1,5 +0,0 @@
-export default (c: echo.Context) => {
-	const name = c.pathParam('name')
-
-	return c.json(200, { message: `Hello, ${name}` })
-}
--- pb/src/entries/index.pb.ts
+++
@@ -1,3 +0,0 @@
-onAfterBootstrap(() => require(`${__hooks}/boot-message.js`).default())
-
-routerAdd(`GET`, `/api/hello/:name`, (c) => require(`${__hooks}/hello.js`).default(c))
